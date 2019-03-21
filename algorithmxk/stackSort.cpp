80d8f2c7ff14f5e4696f471e94578545baa146d4
xk <1365622069@qq.com>
2019-03-21 21:23:53 +0800

    this is one stacksort

---
+++ algorithmxk/stackSort.cpp
@@ -0,0 +1,36 @@
+#include <stack>
+
+int stackSort(std::stack<int> R)
+{
+	std::stack<int> S;
+	
+	//ªÒ»°R
+	int t = R.top();
+	R.pop();
+	while (!R.empty())
+	{
+		if (S.empty() || S.top() <= t)
+		{
+			S.push(t);
+			t = R.top();
+			R.pop();
+		}
+		else
+		{
+			R.push(S.top());
+			S.pop();
+		}
+	}
+	S.push(t);
+	return 0;
+
+}
+
+int main()
+{
+	std::stack<int> R;
+	R.push(1);
+	R.push(2);
+	R.push(1);
+	stackSort(R);
+}
